a0863b1a93e812d5b47ec7ea03fefc7c6a745c11
azaelreyes <azaelreyes20.ar@gmail.com>
2022-11-10 22:55:02 -0800

    created button to change size of sketching pad, added color when mouse is on grid

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const divContainer = document.getElementById("container");
 
-for (let i = 0; i < 256; i++) {
-    const divGrid = document.createElement('div');
-    divGrid.className ="divGrid";
-
-    divGrid.addEventListener('mouseover', (event) => {
-        divGrid.classList.add("hoverClass");
-
-        setTimeout(() => {
-            divGrid.classList.remove("hoverClass");
-          }, "1000")
-
-    });
 
 
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    const size = prompt("Enter # of squares per side for Sketch? (1-100)");
+    
+    for (let i = 0; i < (size*size); i++) {
+        const divGrid = document.createElement('div');
+        divGrid.className ="divGrid";
+
+        const divDimension = 800/size;
+
+        divGrid.style.width = divDimension + "px";
+        divGrid.style.height = divDimension + "px";
+    
+        divGrid.addEventListener('mouseover', (event) => {
+            divGrid.classList.add("hoverClass");
+    
+            setTimeout(() => {
+                divGrid.classList.remove("hoverClass");
+              }, "3000")
+    
+        });
+        divContainer.appendChild(divGrid);
+    };
 
 
-
-    divContainer.appendChild(divGrid);
-};
+  });
 
